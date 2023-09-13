94402330fb66e8a1957e0396b6c6d4712eca6b71
hannafjon <hanna.katle.fjon@gmail.com>
2023-09-13 19:31:50 +0200

    create getters/setters and nameval for login

--- src/main/java/app/login.java
+++ src/main/java/app/login.java
@@ -2,4 +2,37 @@ package app;
 
 public class Login {
   
+  private String username; 
+  private String output; 
+
+  public void setUsername(String username){ 
+    this.username = username; 
+  }
+
+  public String getUsername(){
+    return username; 
+  }
+
+  public void setOutput(){
+    this.output = "Invalid username"; 
+  }
+
+  public String getOutput(){
+    return output; 
+  }
+
+  public boolean validateUsername(String username){
+    if(((username == null))||(username.length() > 8)){
+      return false; 
+    }
+    else{
+      return true;
+    } 
+  }
+
+
+
+
+  
+  
 }
