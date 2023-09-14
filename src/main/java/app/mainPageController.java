ee21a1eff8be899b30592dab9aa19a3d7072f4ce
hannafjon <hanna.katle.fjon@gmail.com>
2023-09-14 13:57:15 +0200

    Initialise "LogOut"-Button

--- src/main/java/app/mainPageController.java
+++ src/main/java/app/mainPageController.java
@@ -2,9 +2,15 @@ package app;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class mainPageController {
 
@@ -24,12 +30,39 @@ public class mainPageController {
     private Button randomeRecepieButton;
 
     @FXML
+    private Button logOutButton;
+
+    @FXML
     void initialize() {
         assert headerText != null : "fx:id=\"headerText\" was not injected: check your FXML file 'MainPage.fxml'.";
         assert myRecepiesButton != null : "fx:id=\"myRecepiesButton\" was not injected: check your FXML file 'MainPage.fxml'.";
         assert randomeRecepieButton != null : "fx:id=\"randomeRecepieButton\" was not injected: check your FXML file 'MainPage.fxml'.";
 
     }
+
+    
+
+    public void handelAction(ActionEvent e){
+        
+        if(e.getSource() == logOutButton){
+            
+            try {
+              FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/Login.fxml"));
+              Parent root = loader.load();
+              Scene scene = new Scene(root);
+              Stage stage = (Stage) logOutButton.getScene().getWindow();
+              stage.setScene(scene);
+              stage.show();
+              
+            } catch (Exception a) {
+              a.printStackTrace(); 
+              // TODO: handle exception
+            }
+  
+        }
+  
+      }
+  
 
     public void setheadertext(String username){
         headerText.setText(username + "´s cookbook.");
