075b11c017c241970dc6660a2d67120e40d90abb
Jørgen Fjermedal <jorgen.fjermedal@hotmail.com>
2023-09-15 16:54:29 +0200

    Fixed method bodies for CookBook and User

--- src/main/java/app/CookBook.java
+++ src/main/java/app/CookBook.java
@@ -1,5 +1,26 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CookBook {
+
+  private List<Recipe> recipes;
+
+  public CookBook(List<Recipe> recipes) {
+    this.recipes = recipes;
+  }
+
+  public List<Recipe> getRecipes() {
+  List<Recipe> copy = new ArrayList<>(recipes);
+      return copy;
+  }
+
+  public void setRecipes(List<Recipe> recipes) {
+    this.recipes = recipes;
+  }
+
+  
+  
   
 }
--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -4,12 +4,14 @@ public class User {
   private String username;
   private String password; 
   static final String output="Invalid Username or password, both needs at least 9 characters";
+  private CookBook cookBook;
 
   
 
-  public User(String username, String password) {
+  public User(String username, String password,CookBook cookBook) {
     setUsername(username);
     setPassword(password);
+    this.cookBook=cookBook;
   }
 
   public User(){}
@@ -40,6 +42,15 @@ public class User {
   public boolean validatePassword(String password){
     return (((password != null))&&(password.length() > 8));
   }
+
+  public CookBook getCookBook() {
+    return cookBook;
+  }
+
+  public void setCookBook(CookBook cookBook) {
+    this.cookBook = cookBook;
+  }
+  
   
   
 }
