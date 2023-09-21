cb86d9d5a9a9a3df5d62642e4a7b16234de58cba
Jørgen Fjermedal <jorgen.fjermedal@hotmail.com>
2023-09-21 12:13:13 +0200

    moved all files over into modules-template

---
+++ core/src/main/java/core/CookBook.java
@@ -0,0 +1,47 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CookBook {
+
+  private List<Recipe> recipes;
+
+  public CookBook(List<Recipe> recipes) {
+    this.recipes = recipes;
+  }
+
+  public List<Recipe> getRecipes() {
+  List<Recipe> copy = new ArrayList<>(recipes);
+      return copy;
+  }
+
+  public void setRecipes(List<Recipe> recipes) {
+    this.recipes = recipes;
+  }
+
+  public void addRecipe(Recipe recipe){
+    recipes.add(recipe);
+
+  }
+
+  public void removeRecipe(Recipe recipe){
+    if(recipes.contains(recipe)) recipes.remove(recipe);
+    else{
+      throw new IllegalArgumentException("Recipe not in cookbook");
+    }
+  }
+
+  
+
+  @Override
+  public String toString() {
+    return "CookBook [recipes=" + recipes + "]";
+  }
+
+  
+
+  
+  
+  
+}
---
+++ core/src/main/java/core/Ingredient.java
@@ -0,0 +1,90 @@
+package core;
+
+public class Ingredient {
+
+  private String name;
+  private double amount;
+  private String unit;
+  public final static String GRAMS ="g", DL="dl", PIECES="pieces";
+
+
+
+public Ingredient(){
+
+}
+
+  public Ingredient(String name, double amount, String unit) {
+    setName(name);
+    setAmount(amount);
+    if(isValidUnit(unit)){
+      this.unit=unit;
+    }
+    else{
+      throw new IllegalArgumentException("Not a valid unit of measurement! Legal ones are g, dl and pieces");
+    }
+    
+  }
+
+
+
+  public String getName() {
+    return name;
+  }
+
+  private boolean isValidUnit(String unit) {
+    return unit.equals(GRAMS) || unit.equals(DL) || unit.equals(PIECES);
+}
+
+
+
+  public void setName(String name) {
+    if(name==null) throw new IllegalArgumentException("Ingredient needs a name");
+    if(name.equals("")){
+      throw new IllegalArgumentException("Ingredient needs a name");
+    }
+    this.name = name;
+  }
+
+
+
+  public double getAmount() {
+    return amount;
+  }
+
+
+
+  public void setAmount(double amount) {
+    if(amount <= 0){
+      throw new IllegalArgumentException("Must have more than 0 of the ingredient");
+    }
+    this.amount = amount;
+  }
+
+
+
+  public String getUnit() {
+    return unit;
+  }
+
+  public void setUnit(String unit){
+    if(isValidUnit(unit)){
+      this.unit=unit;
+    }
+    else{
+      throw new IllegalArgumentException("Not a valid unit of measurement! Legal ones are g, dl and pieces");
+    }
+
+  }
+
+
+
+  @Override
+  public String toString() {
+    return name + ","+ amount +","+ unit;
+  }
+  
+  
+
+
+  
+}
